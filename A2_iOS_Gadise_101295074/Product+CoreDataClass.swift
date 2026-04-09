612e1f113eb59522ffd94eed08415c92197adee3
Gadise oli <113951324+Gadise13@users.noreply.github.com>
2026-04-09 16:28:11 -0500

    Add Product Core Data class

---
+++ A2_iOS_Gadise_101295074/Product+CoreDataClass.swift
@@ -0,0 +1,11 @@
+//
+//   Product+CoreDataClass.swift
+//  A2_iOS_Gadise_101295074
+//
+//  Created by Gadise Oli on 2026-04-09.
+//
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {}
