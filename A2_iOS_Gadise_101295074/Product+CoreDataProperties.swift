07ee18127777b75cdf5d791557c5ac1289013a63
Gadise oli <113951324+Gadise13@users.noreply.github.com>
2026-04-09 16:29:54 -0500

    Add Core Data properties to Product entity

---
+++ A2_iOS_Gadise_101295074/Product+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+
+//
+//Product+CoreDataProperties.swift
+//  A2_iOS_Gadise_101295074
+
+// Created by Gadise Oli on 2026-04-09.
+//
+import Foundation
+import CoreData
+
+extension Product {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Product> {
+        return NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged public var productID: Int64
+    @NSManaged public var productName: String?
+    @NSManaged public var productDescription: String?
+    @NSManaged public var productPrice: Double
+    @NSManaged public var productProvider: String?
+}
+
+extension Product: Identifiable {}
