0ae73cfee1fc652fc282c35bd0a672806b0347a9
Gadise oli <113951324+Gadise13@users.noreply.github.com>
2026-04-09 16:42:55 -0500

    Add README file for project documentation

---
+++ A2_iOS_Gadise_101295074/Add a README.m
@@ -0,0 +1,16 @@
+# A2 iOS Product App
+
+Name: Gadise Oli  
+Student ID: 101295074  
+
+## GitHub Repository
+https://github.com/Gadise13/A2_iOS_Gadise_101295074
+
+## Features
+- Core Data product storage
+- Display first product on launch
+- Product navigation
+- View all products
+- Search by name or description
+- Add new products
+
